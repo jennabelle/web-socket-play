7082cc1cc247ea8912ab874a73d4a81c56efb2be
Jenna Vuong <jennavuong@gmail.com>
2016-08-24 23:45:56 -0700

    add functionality for when user disconnects from chat

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('index.html'); // may have to change to res.sendFile()
 });
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	})
 })
 
 http.listen(3000, function(){
